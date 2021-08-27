4d8ec9b3fc55a175906ca6e05bb8376ad897245c
Jon Rutter <contact@jonrutter.com>
2021-08-27 18:32:54 -0400

    Updated App component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
+import React from 'react';
+
+// components
+import AppBox from './layout/AppBox';
+import FormSection from './layout/FormSection';
+import ResultSection from './layout/ResultSection';
+
 function App() {
   return (
-    <div>
-      <h1>Hello, world!</h1>
+    <div className="site-wrapper">
+      <header>
+        <h1 className="site-title">
+          <span>SPLI</span>
+          <span>TTER</span>
+        </h1>
+      </header>
+      <AppBox>
+        <FormSection />
+        <ResultSection />
+      </AppBox>
     </div>
   );
 }
