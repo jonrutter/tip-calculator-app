0a4e9751ee97d17ffce73eef2c29fb35feb66984
Jon Rutter <contact@jonrutter.com>
2021-08-27 18:32:43 -0400

    Created basic app layout components

---
+++ src/layout/FormSection/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+// styles
+import s from './FormSection.module.css';
+
+const FormSection = () => {
+  return (
+    <div className={s.wrapper}>
+      <div className={s.content}>
+        <h2>Form Section</h2>
+      </div>
+    </div>
+  );
+};
+
+export default FormSection;
---
+++ src/layout/ResultSection/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+// styles
+import s from './ResultSection.module.css';
+
+const ResultSection = () => {
+  return (
+    <div className={s.wrapper}>
+      <div className={s.content}>
+        <h2>Result Section</h2>
+      </div>
+    </div>
+  );
+};
+
+export default ResultSection;
