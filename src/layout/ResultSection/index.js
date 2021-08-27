dbed027f6c8b1486ad72babca959ce4bc61e6a8d
Jon Rutter <contact@jonrutter.com>
2021-08-27 19:15:12 -0400

    Result section

--- src/layout/ResultSection/index.js
+++ src/layout/ResultSection/index.js
@@ -3,11 +3,40 @@ import React from 'react';
 // styles
 import s from './ResultSection.module.css';
 
-const ResultSection = () => {
+const ResultSection = ({
+  tipAmount = '0.00',
+  total = '0.00',
+  formIsEmpty = true,
+  resetForm,
+}) => {
   return (
     <div className={s.wrapper}>
       <div className={s.content}>
-        <h2>Result Section</h2>
+        <section className={s.row}>
+          <div className={s.col}>
+            <h2 className={s.title}>Tip Amount</h2>
+            <p className={s.subtitle}>/ person</p>
+          </div>
+          <div className={s.col}>
+            <span className={s.price}>${tipAmount}</span>
+          </div>
+        </section>
+        <section className={s.row}>
+          <div className={s.col}>
+            <h2 className={s.title}>Total</h2>
+            <p className={s.subtitle}>/ person</p>
+          </div>
+          <div className={s.col}>
+            <span className={s.price}>${total}</span>
+          </div>
+        </section>
+        <button
+          onClick={resetForm}
+          className={formIsEmpty ? `${s.resetBtn} ${s.disabled}` : s.resetBtn}
+          disabled={formIsEmpty}
+        >
+          Reset
+        </button>
       </div>
     </div>
   );
